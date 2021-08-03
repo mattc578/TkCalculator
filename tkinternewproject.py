0b791119b2bce8c4fdbb55ddcf4b6cd27547ced8
Mattcoding4L <thisiscringeyaf@gmail.com>
2021-08-03 10:05:46 -0400

    Slideshow of the top 5 most common tigers, intro to tkinter

---
+++ tkinternewproject.py
@@ -0,0 +1,118 @@
+from tkinter import *
+from PIL import ImageTk
+
+root = Tk()
+root.title('Facts about Different types of Tigers')
+tigericon = PhotoImage(file='C:\\tigericon.png')
+root.iconphoto(True, tigericon)
+
+siberian = ImageTk.PhotoImage(file='C:\\tiger1siberian.png')
+bengal = ImageTk.PhotoImage(file='C:\\tiger2bengal.png')
+indochinese = ImageTk.PhotoImage(file='C:\\tiger3indochinese.png')
+malayan = ImageTk.PhotoImage(file='C:\\tiger4malayan.png')
+sumatran = ImageTk.PhotoImage(file='C:\\tiger5sumatram.png')
+
+tigers = [bengal, indochinese, malayan, siberian, sumatran]
+
+currenttiger = Label(root, image=bengal, height=600, compound='bottom', width=1000)
+currenttigerinfo = Label(root, text='Bengal Tiger', font=('Arial', 35, 'bold'))
+currenttiger.grid(row=0, column=0, columnspan=3)
+currenttigerinfo.grid(row=0, column=0, columnspan=3)
+
+
+def forwardbutton(imagenum):
+    global currenttiger
+    global tigers
+    global currenttigerinfo
+    global buttonforward
+    global currentimage
+    global buttonforward
+    currentimage = imagenum
+    currenttigerinfo.grid_forget()
+    currenttiger.grid_forget()
+    currenttiger = Label(root, image=tigers[imagenum], height=600, width=1000)
+    currenttiger.grid(row=0, column=0, columnspan=3)
+    if imagenum == 1:
+        currenttigerinfo = Label(root, text='Indochinese Tiger', font=('Arial', 35, 'bold'))
+        currenttigerinfo.grid(row=0, column=0, columnspan=3)
+        buttonforward = Button(root, text='→', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                               command=lambda: forwardbutton(imagenum + 1))
+        buttonforward.grid(row=1, column=2)
+    if imagenum == 2:
+        currenttigerinfo = Label(root, text='Malayan Tiger', font=('Arial', 35, 'bold'))
+        currenttigerinfo.grid(row=0, column=0, columnspan=3)
+        buttonforward = Button(root, text='→', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                               command=lambda: forwardbutton(imagenum + 1))
+        buttonforward.grid(row=1, column=2)
+    if imagenum == 3:
+        currenttigerinfo = Label(root, text='Siberian Tiger', font=('Arial', 35, 'bold'))
+        currenttigerinfo.grid(row=0, column=0, columnspan=3)
+        buttonforward = Button(root, text='→', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                               command=lambda: forwardbutton(imagenum + 1))
+        buttonforward.grid(row=1, column=2)
+    elif imagenum == 4:
+        currenttigerinfo = Label(root, text='Sumatran Tiger', font=('Arial', 35, 'bold'))
+        currenttigerinfo.grid(row=0, column=0, columnspan=3)
+        buttonforward = Button(root, text='→', state=DISABLED)
+        buttonforward.grid(row=1, column=2)
+
+    buttonbackward = Button(root, text='←', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3, command=lambda: backclick(imagenum-1))
+    buttonbackward.grid(row=1, column=0)
+
+def backclick(imagenum):
+    global buttonbackward
+    global tigers
+    global currenttigerinfo
+    global currenttiger
+    global buttonforward
+    currenttiger.grid_forget()
+    currenttigerinfo.grid_forget()
+    if imagenum == 0:
+        buttonbackward = Button(root, text='←', state=DISABLED)
+        buttonbackward.grid(row=1, column=0)
+        currenttiger = Label(root, image=tigers[0], height=600, width=1000)
+        currenttigerinfo = Label(root, text='Bengal Tiger', font=('Arial', 35, 'bold'))
+        currenttigerinfo.grid(row=0, column=0, columnspan=3)
+        currenttiger.grid(row=0, column=0, columnspan=3)
+    elif imagenum > 0:
+        currenttiger = Label(root, image=tigers[imagenum], height=600, width=1000)
+        currenttiger.grid(row=0, column=0, columnspan=3)
+        if imagenum == 1:
+            currenttigerinfo = Label(root, text='Indochinese Tiger', font=('Arial', 35, 'bold'))
+            currenttigerinfo.grid(row=0, column=0, columnspan=3)
+            buttonbackward = Button(root, text='←', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                                    command=lambda: backclick(imagenum - 1))
+            buttonbackward.grid(row=1, column=0)
+        if imagenum == 2:
+            currenttigerinfo = Label(root, text='Malayan Tiger', font=('Arial', 35, 'bold'))
+            currenttigerinfo.grid(row=0, column=0, columnspan=3)
+            buttonbackward = Button(root, text='←', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                                    command=lambda: backclick(imagenum - 1))
+            buttonbackward.grid(row=1, column=0)
+        if imagenum == 3:
+            currenttigerinfo = Label(root, text='Siberian Tiger', font=('Arial', 35, 'bold'))
+            currenttigerinfo.grid(row=0, column=0, columnspan=3)
+            buttonbackward = Button(root, text='←', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                                    command=lambda: backclick(imagenum - 1))
+            buttonbackward.grid(row=1, column=0)
+        elif imagenum == 4:
+            currenttigerinfo = Label(root, text='Sumatran Tiger', font=('Arial', 35, 'bold'))
+            currenttigerinfo.grid(row=0, column=0, columnspan=3)
+            buttonbackward = Button(root, text='←', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                                    command=lambda: backclick(imagenum - 1))
+            buttonbackward.grid(row=1, column=0)
+    buttonforward = Button(root, text='→', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3,
+                           command=lambda: forwardbutton(imagenum + 1))
+    buttonforward.grid(row=1, column=2)
+
+
+
+
+buttonforward = Button(root, text='→', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3, command=lambda: forwardbutton(1))
+buttonforward.grid(row=1, column=2)
+buttonquit = Button(root, text='Quit', fg='black', bg='#d3d3d3', width=10, height=3, padx=5, pady=3, command=root.quit)
+buttonquit.grid(row=1, column=1)
+buttonbackward = Button(root, text='←', fg='black', bg='#d3d3d3', width=15, height=3, padx=5, pady=3, command=lambda: backclick(0), state=DISABLED)
+buttonbackward.grid(row=1, column=0)
+
+root.mainloop()
--- tkinterprojects.py
+++ tkinterprojects.py
@@ -1,12 +1,18 @@
 from tkinter import *
 import random
+from PIL import ImageTk
+
+'''implement pictures in buttons, and make it look better'''
 
 root = Tk()
 root.title('Rock Paper Scissors')
 e = Entry(root, width=50)
 e.grid(row=0, column=0, columnspan=3)
-window_icon = PhotoImage(file='C:\\rockpaperscissorstk.png')
-root.iconphoto(True, window_icon)
+root.iconphoto(True, PhotoImage(file='C:\\rockpaperscissorstk.png'))
+
+myImage = ImageTk.PhotoImage(file='C:\\rockpaperscissorstk.png')
+iconlabel = Label(root, image=myImage)
+iconlabel.grid(row=7, column=0, rowspan=3, columnspan=3)
 
 def choserock():
     global player_move
@@ -24,7 +30,14 @@ def chosescissor():
     player_move = 'scissor'
     e.insert(0, player_move)
 
+times_won = 0
+times_lost = 0
+times_drew = 0
+
 def play():
+    global times_won
+    global times_lost
+    global times_drew
     moves = ['rock', 'paper', 'scissor']
     cpu_move = random.choice(moves)
     e.delete(0, END)
@@ -34,33 +47,53 @@ def play():
         results = 'You lost, loser'
         cpumovelabel = Label(root, text=results, width=20, font=('Arial', 30, 'bold'))
         cpumovelabel.grid(row=2, column=0, columnspan=3)
+        times_lost += 1
     if cpu_move == 'rock' and player_move == 'paper':
         results = "Congrats you're a winner!"
         cpumovelabel = Label(root, text=results, width=20, font=('Arial', 30, 'bold'))
         cpumovelabel.grid(row=2, column=0, columnspan=3)
+        times_won += 1
     if cpu_move == 'paper' and player_move == 'rock':
         results = 'You lost, loser'
         cpumovelabel = Label(root, text=results, width=20, font=('Arial', 30, 'bold'))
         cpumovelabel.grid(row=2, column=0, columnspan=3)
+        times_lost += 1
     if cpu_move == 'paper' and player_move == 'scissor':
         results = "Congrats you're a winner!"
         cpumovelabel = Label(root, text=results, width=20, font=('Arial', 30, 'bold'))
         cpumovelabel.grid(row=2, column=0, columnspan=3)
+        times_won += 1
     if cpu_move == 'scissor' and player_move == 'rock':
         results = "Congrats you're a winner!"
         cpumovelabel = Label(root, text=results, width=20, font=('Arial', 30, 'bold'))
         cpumovelabel.grid(row=2, column=0, columnspan=3)
+        times_won += 1
     if cpu_move == 'scissor' and player_move == 'paper':
         results = 'You lost, loser'
         cpumovelabel = Label(root, text=results, width=20, font=('Arial', 30, 'bold'))
         cpumovelabel.grid(row=2, column=0, columnspan=3)
+        times_lost += 1
     if cpu_move == player_move:
         results = 'It was a tie game!'
         cpumovelabel = Label(root, text=results, width=20, font=('Arial', 30, 'bold'))
         cpumovelabel.grid(row=2, column=0, columnspan=3)
+        times_drew += 1
+    results_label = Label(root, text='total wins: %i\n total losses: %i\n total draws: %i' %(times_won, times_lost, times_drew),
+                          fg='black', bg='#d3d3d3', width=15, height=4)
+    results_label.grid(row=0, column=0)
 
 def restart():
+    global times_won
+    global times_lost
+    global times_drew
     e.delete(0, END)
+    times_lost = 0
+    times_won = 0
+    times_drew = 0
+    results_label = Label(root, text='total wins: %i\n total losses: %i\n total draws: %i' % (
+    times_won, times_lost, times_drew),
+                          fg='black', bg='#d3d3d3', width=15, height=4)
+    results_label.grid(row=0, column=0)
 
 button_rock = Button(root, text='Rock', fg='black', bg='#d3d3d3', width=30, height=10, padx=5, pady=3, command=choserock)
 button_rock.grid(row=3, column=0)
@@ -74,5 +107,8 @@ button_play.grid(row=1, column=0, columnspan=3)
 
 button_restart = Button(root, text='Restart Game', fg='black', bg='#d3d3d3', width=15, height=1, padx=5, pady=3, command=restart)
 button_restart.grid(row=4, column=0, columnspan=3)
+
+button_quit = Button(root, text='Quit the Game', fg='black', bg='#d3d3d3', width=10, height=1, padx=5, pady=3, command=root.quit)
+button_quit.grid(row=0, column=2, columnspan=3)
 
 root.mainloop()
